1ed5a46d997ca7d7940b62225509447b0fe75f64
Sam Mason <smason@cloudflare.com>
2018-12-04 17:30:04 +0000

    Add percent sign

--- components/ColorBlindFilter.js
+++ components/ColorBlindFilter.js
@@ -3,18 +3,32 @@ import React from "react"
 const filters = [
   { label: "None", value: "none", population: "92%" },
   { label: "Deuteranomaly", value: "deuteranomaly", population: "2.7%" },
-  { label: "Protanomaly", value: "protanomaly", population: "0.66"},
-  { label: "Protanopia", value: "protanopia", population: "0.59%"},
-  { label: "Deuteranopia", value: "deuteranopia", population: "0.56%"},
-  { label: "Tritanopia", value: "tritanopia", population: "0.016%"},
+  { label: "Protanomaly", value: "protanomaly", population: "0.66%" },
+  { label: "Protanopia", value: "protanopia", population: "0.59%" },
+  { label: "Deuteranopia", value: "deuteranopia", population: "0.56%" },
+  { label: "Tritanopia", value: "tritanopia", population: "0.016%" },
   { label: "Tritanomaly", value: "tritanomaly", population: "0.01%" },
   { label: "Achromatopsia", value: "achromatopsia", population: "<0.0001%" },
   { label: "Achromatomaly", value: "achromatomaly", population: "Unknown %" }
 ]
 
-const SingleRadio = ({ label, value, population, onChange, currentValue, ...props }) => (
-  <Div display="flex" alignItems="flex-start" width={1} flexWrap='wrap' mb={2} {...props}>
-    <Div width='1rem' mr={1}>
+const SingleRadio = ({
+  label,
+  value,
+  population,
+  onChange,
+  currentValue,
+  ...props
+}) => (
+  <Div
+    display="flex"
+    alignItems="flex-start"
+    width={1}
+    flexWrap="wrap"
+    mb={2}
+    {...props}
+  >
+    <Div width="1rem" mr={1}>
       <Input
         type="radio"
         checked={currentValue === value}
@@ -25,18 +39,22 @@ const SingleRadio = ({ label, value, population, onChange, currentValue, ...prop
       />
     </Div>
     <Label pl={1} htmlFor={label}>
-      <Span>{label}</Span> 
+      <Span>{label}</Span>
     </Label>
-    <Span ml='auto' fontSize={1} lineHeight={1.5}><b>{population}</b></Span>
+    <Span ml="auto" fontSize={1} lineHeight={1.5}>
+      <b>{population}</b>
+    </Span>
   </Div>
 )
 
 const ColorBlindFilter = ({ onChange, currentValue }) => {
   return (
     <Div display="flex" flexWrap="wrap">
-      <H4 width={1} mb={2} mt={4} display='flex'>
+      <H4 width={1} mb={2} mt={4} display="flex">
         <Span>Color Blindness Filter</Span>
-        <Span ml='auto' fontSize={1}>Population</Span>
+        <Span ml="auto" fontSize={1}>
+          Population
+        </Span>
       </H4>
 
       {filters.map(f => (
