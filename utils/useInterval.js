10615dc461c66dbc2cbdbecfdb68ba334c98c027
Sam Mason <smason@cloudflare.com>
2018-12-04 16:42:44 +0000

    Fix pinning while animating

--- utils/useInterval.js
+++ utils/useInterval.js
@@ -4,21 +4,18 @@ const useInterval = ({ startImmediate = false, duration, callback }) => {
   const [intervalState, setIntervalState] = useState(startImmediate)
   const intervalRef = useRef()
 
-  useEffect(
-    () => {
-      if (intervalState) {
-        const intervalId = setInterval(() => {
-          callback && callback()
-        }, duration)
-        intervalRef.current = intervalId
-      }
+  useEffect(() => {
+    if (intervalState) {
+      const intervalId = setInterval(() => {
+        callback && callback()
+      }, duration)
+      intervalRef.current = intervalId
+    }
 
-      return () => {
-        clearInterval(intervalRef.current)
-      }
-    },
-    [intervalState]
-  )
+    return () => {
+      clearInterval(intervalRef.current)
+    }
+  })
   return {
     isRunning: intervalState,
     start: () => {
