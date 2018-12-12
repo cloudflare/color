02b74482e2e4746db4440dcfb12a6f2711c0aa42
mrmrs <hi@mrmrs.cc>
2018-12-12 14:39:53 +0000

    Adding more about info to color tool

--- pages/about.js
+++ pages/about.js
@@ -2,16 +2,48 @@ import React from "react"
 
 const About = props => (
   <Div>
-    <Header bg='indigo.3' color='white' px={5} py={6}>
-      <H2 mt={0} fontSize={7} mb={3} color='indigo.9'>Visualizing Color</H2>
-      <P fontSize={4} color='indigo.9'>A documentation journey</P>
-    </Header>
-    <Div p={5}>
-    <H3 fontSize={5}>The Question</H3>
-    <P fontSize={4}>
-      What are all the possible things someone might need to know about how and when to use a color? 
-    </P>
-  </Div>
+    <Section height='80vh' color='gray.0' px={5} py={6} display='flex' flexWrap='wrap' alignItems='center' borderBottom='1px solid' borderColor='gray.8'>
+      <Header width={1}>
+        <H2 mt={0} fontSize={[5,6,9]} fontWeight={700} mb={3} >Thinking about color</H2>
+        <P fontSize={[4,5]}>A documentation journey with the Product Design & Engineering teams at Cloudflare</P>
+      </Header>
+    </Section>
+    <Section height='80vh' color='gray.0' px={5} py={6} display='flex' flexWrap='wrap' alignItems='center' borderBottom='1px solid' borderColor='gray.8'>
+      <Div>
+        <H3 fontSize={[6,7,8]} mb={2}>The Question</H3>
+        <P fontSize={5}>
+          What are all the possible things someone might want or need to know about a color?
+        </P>
+        <Ul display='none'>
+          <Li>String Name</Li>
+          <Li>Hue Name</Li>
+          <Li>Saturation</Li>
+          <Li>Alpha level</Li>
+          <Li>
+            Values 
+            <Ul>
+              <Li>LAB</Li>
+              <Li>RGB(A)</Li>
+              <Li>HSL(A)</Li>
+              <Li>HSV</Li>
+              <Li>uiColor</Li>
+            </Ul>
+          </Li>
+          <Li>What will this look like?</Li>
+          <Li>What will this look like as a button?</Li>
+          <Li>What will this look like as a border?</Li>
+          <Li>What will this look like as a background?</Li>
+          <Li>What will this look like as text?</Li>
+          <Li>What colors will look good with this that are also accessible?</Li>
+          <Li>How should I use this color</Li>
+          <Li>Is this color accessible with white?</Li>
+          <Li>Is this color accessible with black?</Li>
+          <Li>Where do we use this color?</Li>
+          <Li>What's the next darkest?</Li>
+          <Li>What's the next lightest?</Li>
+        </Ul>
+      </Div>
+    </Section>
   </Div>
 )
 
