24d01155a5eedf62874b325b0a400c13af219aee
Sam Mason <smason@cloudflare.com>
2018-12-13 14:31:27 +0000

    Setup meta tag for responsive and setup now scripts for deploying

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,7 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <style dangerouslySetInnerHTML={{ __html: this.props.css }} />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />
