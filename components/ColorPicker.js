c6bd04d701dcaa43ce55dc2d69925887a4cf3049
Sam Mason <smason@cloudflare.com>
2018-12-19 16:50:00 +0000

    Setup styled slider

--- components/ColorPicker.js
+++ components/ColorPicker.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Color from "color"
+import theme from "../theme"
 
 const getHex = val => {
   try {
@@ -135,7 +136,8 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
   return (
     <Article
       px={4}
-      py={4} color={
+      py={4}
+      color={
         Color(currentColor).isLight()
           ? "rbga(0,0,0,.75)"
           : "rgba(255,255,255,.85)"
@@ -146,55 +148,62 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
       flexWrap="wrap"
     >
       <Flex width={1} mb={4}>
-          <TextInput
-            color="inherit"
-            bg="transparent"
-            border={0}
-            fontSize={4}
-            fontWeight={800}
-            value={colorValues.hex}
-            onChange={handleHexChange}
-            px={0}
-            css={{ transition: "none" }}
-          />
-          <P
-            ml='auto'
-            mr={3}
-            display='inline-block'
-            fontSize={1}
-            px={2}
-            py={1}
-            borderRadius={9999}
-            my={0}
-            bg='black'
-            color={currentColor}
-            css={{ transition: "none", whiteSpace: 'nowrap' }}
-          >
-            <Span fontWeight={800}>{contrastForBlack(currentColor)} </Span>
+        <TextInput
+          color="inherit"
+          bg="transparent"
+          border={0}
+          fontSize={4}
+          fontWeight={800}
+          value={colorValues.hex}
+          onChange={handleHexChange}
+          px={0}
+          css={{ transition: "none" }}
+        />
+        <P
+          ml="auto"
+          mr={3}
+          display="inline-block"
+          fontSize={1}
+          px={2}
+          py={1}
+          borderRadius={9999}
+          my={0}
+          bg="black"
+          color={currentColor}
+          css={{ transition: "none", whiteSpace: "nowrap" }}
+        >
+          <Span fontWeight={800}>{contrastForBlack(currentColor)} </Span>
 
-            <Span>{showContrastLevel(contrastForBlack(currentColor))}</Span>
-          </P>
-          <P
-            display='inline-block'
-            fontSize={1}
-            px={2}
-            py={1}
-            borderRadius={9999}
-            m={0}
-            bg='white'
-            color={currentColor}
-            css={{ transition: "none", whiteSpace: 'nowrap' }}
-          >
-            <Span fontWeight={800}>{contrastForWhite(currentColor)} </Span>
-            <Span>{showContrastLevel(contrastForWhite(currentColor))}</Span>
-          </P>
-        </Flex> 
+          <Span>{showContrastLevel(contrastForBlack(currentColor))}</Span>
+        </P>
+        <P
+          display="inline-block"
+          fontSize={1}
+          px={2}
+          py={1}
+          borderRadius={9999}
+          m={0}
+          bg="white"
+          color={currentColor}
+          css={{ transition: "none", whiteSpace: "nowrap" }}
+        >
+          <Span fontWeight={800}>{contrastForWhite(currentColor)} </Span>
+          <Span>{showContrastLevel(contrastForWhite(currentColor))}</Span>
+        </P>
+      </Flex>
       <Flex width={1}>
-        <Flex flexWrap='wrap' width={1} mb={1}>
-          <Label fontSize={2} display='block' width={1} color="inherit" fontWeight={700} css={{ transition: "none" }}>
+        <Flex flexWrap="wrap" width={1} mb={1}>
+          <Label
+            fontSize={2}
+            display="block"
+            width={1}
+            color="inherit"
+            fontWeight={700}
+            css={{ transition: "none" }}
+          >
             Red
           </Label>
-          <Div width={3/4}>
+          <Div width={3 / 4}>
             <Input
               name="r"
               type="range"
@@ -204,9 +213,24 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               value={colorValues.rgb.r}
               onChange={handleRGBChange}
               width={1}
+              css={{
+                appearance: "none",
+                borderRadius: "999px",
+                height: "6px",
+                outline: "none",
+                opacity: 0.6,
+                "&::-webkit-slider-thumb": {
+                  backgroundColor: theme.colors.gray[4],
+                  appearance: "none",
+                  width: "16px",
+                  height: "16px",
+                  borderRadius: "50%",
+                  cursor: "pointer"
+                }
+              }}
             />
           </Div>
-          <Div width={1/4} justifyContent='right'>
+          <Div width={1 / 4} justifyContent="right">
             <TextInput
               px={0}
               py={0}
@@ -214,7 +238,7 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               border={0}
               color="inherit"
               bg="transparent"
-              textAlign='right'
+              textAlign="right"
               width={1}
               min="0"
               max="255"
@@ -227,11 +251,17 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
           </Div>
         </Flex>
       </Flex>
-      <Flex width={1} flexWrap='wrap' mb={1}>
-        <Label width={1} fontSize={2} fontWeight={700} color="inherit" css={{ transition: "none" }}>
+      <Flex width={1} flexWrap="wrap" mb={1}>
+        <Label
+          width={1}
+          fontSize={2}
+          fontWeight={700}
+          color="inherit"
+          css={{ transition: "none" }}
+        >
           Green
         </Label>
-        <Div width={3/4}>
+        <Div width={3 / 4}>
           <Input
             name="g"
             type="range"
@@ -241,9 +271,24 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
             step="1"
             value={colorValues.rgb.g}
             onChange={handleRGBChange}
+            css={{
+              appearance: "none",
+              borderRadius: "999px",
+              height: "6px",
+              outline: "none",
+              opacity: 0.6,
+              "&::-webkit-slider-thumb": {
+                backgroundColor: theme.colors.gray[4],
+                appearance: "none",
+                width: "16px",
+                height: "16px",
+                borderRadius: "50%",
+                cursor: "pointer"
+              }
+            }}
           />
         </Div>
-        <Div width={1/4} justifyContent='right'>
+        <Div width={1 / 4} justifyContent="right">
           <TextInput
             px={0}
             py={0}
@@ -254,7 +299,7 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
             bg="transparent"
             border={0}
             width={1}
-            textAlign='right'
+            textAlign="right"
             min="0"
             max="255"
             value={Math.floor(colorValues.rgb.g)}
@@ -263,11 +308,17 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
           />
         </Div>
       </Flex>
-      <Flex width={1} flexWrap='wrap'>
-        <Label fontSize={2} width={1} color="inherit" fontWeight={700} css={{ transition: "none" }}>
+      <Flex width={1} flexWrap="wrap">
+        <Label
+          fontSize={2}
+          width={1}
+          color="inherit"
+          fontWeight={700}
+          css={{ transition: "none" }}
+        >
           Blue
         </Label>
-        <Div width={3/4}>
+        <Div width={3 / 4}>
           <Input
             name="b"
             type="range"
@@ -277,9 +328,24 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
             width={1}
             value={colorValues.rgb.b}
             onChange={handleRGBChange}
+            css={{
+              appearance: "none",
+              borderRadius: "999px",
+              height: "6px",
+              outline: "none",
+              opacity: 0.6,
+              "&::-webkit-slider-thumb": {
+                backgroundColor: theme.colors.gray[4],
+                appearance: "none",
+                width: "16px",
+                height: "16px",
+                borderRadius: "50%",
+                cursor: "pointer"
+              }
+            }}
           />
         </Div>
-        <Div width={1/4} justifyContent='right'> 
+        <Div width={1 / 4} justifyContent="right">
           <TextInput
             px={0}
             py={0}
@@ -288,7 +354,7 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
             border={0}
             fontSize={3}
             bg="transparent"
-            textAlign='right'
+            textAlign="right"
             width={1}
             color="inherit"
             min="0"
@@ -300,26 +366,49 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
         </Div>
       </Flex>
       <Flex flexWrap="wrap" mt={3}>
-        <Flex width={1} flexWrap='wrap' mb={1}>
-            <Label width={1} fontSize={2} width={1} fontWeight={700} color="inherit" css={{ transition: "none" }}>
-              Hue
-            </Label>
-          <Div width={3/4}>
-          <Input
+        <Flex width={1} flexWrap="wrap" mb={1}>
+          <Label
             width={1}
-            display='block'
-            name="h"
-            type="range"
-            min="0"
-            max="360"
-            step="1"
-            value={colorValues.hsl.h}
-            onChange={handleHSLChange} />
-        </Div>
-            <Div width={1/4} justifyContent='right'>
+            fontSize={2}
+            width={1}
+            fontWeight={700}
+            color="inherit"
+            css={{ transition: "none" }}
+          >
+            Hue
+          </Label>
+          <Div width={3 / 4}>
+            <Input
+              width={1}
+              display="block"
+              name="h"
+              type="range"
+              min="0"
+              max="360"
+              step="1"
+              value={colorValues.hsl.h}
+              onChange={handleHSLChange}
+              css={{
+                appearance: "none",
+                borderRadius: "999px",
+                height: "6px",
+                outline: "none",
+                opacity: 0.6,
+                "&::-webkit-slider-thumb": {
+                  backgroundColor: theme.colors.gray[4],
+                  appearance: "none",
+                  width: "16px",
+                  height: "16px",
+                  borderRadius: "50%",
+                  cursor: "pointer"
+                }
+              }}
+            />
+          </Div>
+          <Div width={1 / 4} justifyContent="right">
             <TextInput
-            px={0}
-            py={0}
+              px={0}
+              py={0}
               name="h"
               type="number"
               bg="transparent"
@@ -327,7 +416,7 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               border={0}
               fontSize={3}
               width={1}
-              textAlign='right'
+              textAlign="right"
               min="0"
               max="360"
               onChange={handleHSLChange}
@@ -336,11 +425,17 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
             />
           </Div>
         </Flex>
-        <Flex width={1} flexWrap='wrap' mb={1}>
-          <Label width={1} fontSize={2} fontWeight={700} color="inherit" css={{ transition: "none" }}>
+        <Flex width={1} flexWrap="wrap" mb={1}>
+          <Label
+            width={1}
+            fontSize={2}
+            fontWeight={700}
+            color="inherit"
+            css={{ transition: "none" }}
+          >
             Saturation
           </Label>
-          <Div width={3/4}>
+          <Div width={3 / 4}>
             <Input
               name="s"
               type="range"
@@ -350,33 +445,54 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               step="1"
               value={colorValues.hsl.s}
               onChange={handleHSLChange}
+              css={{
+                appearance: "none",
+                borderRadius: "999px",
+                height: "6px",
+                outline: "none",
+                opacity: 0.6,
+                "&::-webkit-slider-thumb": {
+                  backgroundColor: theme.colors.gray[4],
+                  appearance: "none",
+                  width: "16px",
+                  height: "16px",
+                  borderRadius: "50%",
+                  cursor: "pointer"
+                }
+              }}
             />
           </Div>
-          <Div width={1/4}>
-          <TextInput
-            px={0}
-            py={0}
-            name="s"
-            type="number"
-            width={1}
-            textAlign='right'
-            bg="transparent"
-            color="inherit"
-            border={0}
-            fontSize={3}
-            min="0"
-            max="100"
-            onChange={handleHSLChange}
-            value={Math.floor(colorValues.hsl.s)}
-            css={{ transition: "none" }}
-          />
+          <Div width={1 / 4}>
+            <TextInput
+              px={0}
+              py={0}
+              name="s"
+              type="number"
+              width={1}
+              textAlign="right"
+              bg="transparent"
+              color="inherit"
+              border={0}
+              fontSize={3}
+              min="0"
+              max="100"
+              onChange={handleHSLChange}
+              value={Math.floor(colorValues.hsl.s)}
+              css={{ transition: "none" }}
+            />
           </Div>
         </Flex>
-        <Flex width={1} flexWrap='wrap'>
-          <Label width={1} fontSize={2} fontWeight={700} color="inherit" css={{ transition: "none" }}>
+        <Flex width={1} flexWrap="wrap">
+          <Label
+            width={1}
+            fontSize={2}
+            fontWeight={700}
+            color="inherit"
+            css={{ transition: "none" }}
+          >
             Lightness
           </Label>
-          <Div width={3/4}>
+          <Div width={3 / 4}>
             <Input
               name="l"
               type="range"
@@ -386,9 +502,24 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               step="1"
               value={colorValues.hsl.l}
               onChange={handleHSLChange}
+              css={{
+                appearance: "none",
+                borderRadius: "999px",
+                height: "6px",
+                outline: "none",
+                opacity: 0.6,
+                "&::-webkit-slider-thumb": {
+                  backgroundColor: theme.colors.gray[4],
+                  appearance: "none",
+                  width: "16px",
+                  height: "16px",
+                  borderRadius: "50%",
+                  cursor: "pointer"
+                }
+              }}
             />
           </Div>
-          <Div width={1/4} justifyContent='right'>
+          <Div width={1 / 4} justifyContent="right">
             <TextInput
               px={0}
               py={0}
@@ -398,7 +529,7 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               bg="transparent"
               color="inherit"
               border={0}
-              textAlign='right'
+              textAlign="right"
               fontSize={3}
               min="0"
               max="100"
@@ -406,11 +537,15 @@ const ColorPicker = ({ currentColor, onChange, onRemoveColor }) => {
               onChange={handleHSLChange}
               css={{ transition: "none" }}
             />
-            </Div>
+          </Div>
         </Flex>
       </Flex>
-      <Div textAlign='center' mt={3} width={1}>
-        <TextButton bg='transparent' color='inherit' onClick={() => onRemoveColor(currentColor)}>
+      <Div textAlign="center" mt={3} width={1}>
+        <TextButton
+          bg="transparent"
+          color="inherit"
+          onClick={() => onRemoveColor(currentColor)}
+        >
           Remove
         </TextButton>
       </Div>
