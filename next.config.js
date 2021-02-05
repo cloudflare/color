9f7b25defa7c1fc15ee6e3d5cc5f4bcd6fd6f4f9
Sam Mason <smason@cloudflare.com>
2021-02-05 11:11:54 +0000

    Migrate to workers

--- next.config.js
+++ next.config.js
@@ -5,30 +5,29 @@ const webpack = require("webpack")
 const pkg = require("./package.json")
 const withMDX = require("@next/mdx")({
   options: {
-    remarkPlugins: [unwrapImages]
-  }
+    remarkPlugins: [unwrapImages],
+  },
 })
 
 const isProd = process.env.NODE_ENV === "production"
 
-const fetchFiles = filePath => {
+const fetchFiles = (filePath) => {
   const files = fs.readdirSync(filePath)
 
   return files.reduce((acc, file) => {
     const elName = path.basename(file, ".js")
     return {
       ...acc,
-      [elName]: [`${filePath}/${file}`, "default"]
+      [elName]: [`${filePath}/${file}`, "default"],
     }
   }, {})
 }
 
 module.exports = withMDX({
-  target: "serverless",
-  env: {
-    assetPrefix: isProd ? `https://${pkg.name}.cloudflare.design` : ""
-  },
-  assetPrefix: isProd ? `https://${pkg.name}.cloudflare.design` : "",
+  // env: {
+  //   assetPrefix: isProd ? `https://${pkg.name}.cloudflare.design` : "",
+  // },
+  // assetPrefix: isProd ? `https://${pkg.name}.cloudflare.design` : "",
   webpack: (config, {}) => {
     const elements = fetchFiles(path.join(__dirname, "elements"))
     const components = fetchFiles(path.join(__dirname, "components"))
@@ -40,9 +39,9 @@ module.exports = withMDX({
       alias: {
         ...(config.resolve.alias || {}),
         elements: path.resolve(__dirname, "elements/"),
-        components: path.resolve(__dirname, "components/")
-      }
+        components: path.resolve(__dirname, "components/"),
+      },
     }
     return config
-  }
+  },
 })
